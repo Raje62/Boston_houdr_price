de13518a95c419880b6915b18b900f382df1bd48
Raje62 <rajeshwariharsh@iiitp.ac.in>
2024-08-16 02:11:46 +0530

    added new updated html and app files

--- app.py
+++ app.py
@@ -20,7 +20,18 @@ def predict_api():
     print(np.array(list(data.values())).reshape(1,-1))
     new_data = scaler.tranform(np.array(list(data.values())).reshape(1,-1))
     answer = model.predict(new_data)
+    print(answer[0])
     return jsonify(answer[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=model.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
